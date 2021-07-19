c07706fb828de341a4f3def7da529228ed372547
GabrielSouza <gabrieldasilvasouza21@gmail.com>
2021-07-19 09:06:50 -0300

    Adding Problem 2

--- problem2.py
+++ problem2.py
@@ -1,18 +1,23 @@
-counter = 1
-temp = [1,2]
-i = 0
-while i < 4*(10**6):
-	i = temp[0] + temp[1]	
-	if i % 2 == 0:
-		counter += i
-	temp.append(i)
-	temp.pop(0)
-	print(temp,i)
-	if i > 4*(10**6):
-		print("rUNNED HERE")
-		break
-	
+ # Solution to Problem 1 - Multiples 3 and 5
+# - GabrielSilvaSouza
+#
+# Explore more on: 
+# https://github.com/GabrielSilvaSouza/Project-Euler-in-Python 
 
-print(counter)
+def sum_of_even():
+	counter = 2   #Setting the first even
+	temp = [1,2]  #Setting the list that will be use as base to counting
+	i = 0  #while's counter
+	while i < 4000000:
+		i = temp[0] + temp[1]   #receive the two elements from the "temp" list
+		if i % 2 == 0:   #check if it's even
+			counter += i
+		temp.append(i)   #change the elements to the next counting
+		temp.pop(0)
+		
+	return counter
+	
+if __name__ == '__main__':
+	print(sum_of_even())
 		
 
